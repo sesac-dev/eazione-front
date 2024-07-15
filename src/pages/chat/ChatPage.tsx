0d53c7dc1c887413940bf5cc7590d196892ea35b
이승현 <sh980812@naver.com>
2024-07-15 15:19:15 +0900

    feat: BottomNavigation 구현

---
+++ src/pages/chat/ChatPage.tsx
@@ -0,0 +1,11 @@
+import BottomNavigation from '../../components/@common/BottomNavigation';
+
+const ChatPage = () => {
+  return (
+    <div className="">
+      <BottomNavigation />
+    </div>
+  );
+};
+
+export default ChatPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        top: '0 2px 7px 1px rgba(0,0,0,0.1)',
+      },
+    },
   },
   plugins: [],
 };
