8f812fefc3ad5c9b8c3b5c8fa028fbd2908dba80
이승현 <sh980812@naver.com>
2024-07-15 16:21:05 +0900

    feat: my 페이지 기본 UI 구현

--- src/pages/my/MyPage.tsx
+++ src/pages/my/MyPage.tsx
@@ -1,5 +1,16 @@
+import BottomNavigation from '../../components/@common/BottomNavigation';
+import Header from '../../components/@common/Header';
+
 const MyPage = () => {
-  return <div>마이 페이지</div>;
+  return (
+    <>
+      <div className="h-screen w-full">
+        <Header />
+        <div className="h-[calc(100vh-176px)] w-full text-center"></div>
+      </div>
+      <BottomNavigation />
+    </>
+  );
 };
 
 export default MyPage;
