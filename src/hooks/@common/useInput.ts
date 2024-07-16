5196a86beedb67d3626fc9d90afbd6385ae3c168
이승현 <sh980812@naver.com>
2024-07-16 19:41:45 +0900

    feat: input 태그의 상태를 관리 할 custom hook 구현

---
+++ src/hooks/@common/useInput.ts
@@ -0,0 +1,18 @@
+import { useState, ChangeEvent } from 'react';
+
+function useInput<T>(initValue: T): [T, (e: ChangeEvent<HTMLInputElement>) => void, () => void] {
+  const [values, setValues] = useState<T>(initValue);
+
+  const changer = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const init = () => {
+    setValues(initValue);
+  };
+
+  return [values, changer, init];
+}
+
+export default useInput;
