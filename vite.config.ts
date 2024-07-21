1de8ede988c204a0759387124f9694987ec034a1
이승현 <sh980812@naver.com>
2024-07-21 21:33:53 +0900

    fix: 빌드 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import babel from 'vite-plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    commonjs(),
     babel({
       babelConfig: {
         plugins: ['@babel/plugin-transform-runtime'],
