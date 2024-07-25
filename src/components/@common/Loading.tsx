f5ed978f93f4f42b5991cdf58b721312fe556c68
이승현 <sh980812@naver.com>
2024-07-25 19:17:51 +0900

    feat: 서류 스캔 로딩 컴포넌트 구현

---
+++ src/components/@common/Loading.tsx
@@ -0,0 +1,26 @@
+import Lottie from 'lottie-react';
+import docsLoading from '../../assets/lotties/docsLoading.json';
+
+const Loading = () => {
+  const docsLoadingOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: docsLoading,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <div className="absolute left-0 top-0 z-50 h-full w-full bg-black/70">
+      <div className="flex h-full w-full flex-col items-center justify-center gap-5">
+        <Lottie {...docsLoadingOptions} width={70} height={70} />
+        <p>
+          서류 스캔중입니다 <br /> 잠시만 기다려주세요
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
